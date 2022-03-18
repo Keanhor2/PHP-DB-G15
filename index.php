cbe2641d7dd3e1b461dbb7b439b517a2d4f8ebbc
KEA.NHOR <KEA.NHOR@PNC.LAN>
2022-03-18 09:43:14 +0700

    #13 change profile and cover

--- index.php
+++ index.php
@@ -8,12 +8,21 @@ require_once "templates/header.php";
         <div class="profile_header">
            <div class="cover_profile">
                <img class="cover_pro_size" src="images/bg_img1.jpg" alt="">
-               <div class="change_cover"> <input type="file" class="image" name="image_cover"> </input> </div>
+               <div class="change_cover"> 
+                    <label>
+                        <i class="fa fa-image fs-2 text-dark "></i>
+                        <input type="file" name="myfile" style="display:none">
+                    </label> 
+                </div>
            </div>
            <div class="profile_info">
                <div class="profile_logo">
                     <img class="profile_logo_size" src="images/lotus_img1.jpg" alt="">
-                    <div class="change_profile"><input type="file" class="ms-3" name="image_profile">
+                        <div class="change_profile">
+                        <label>
+                            <i class="fa fa-camera fs-3 "></i>
+                            <input  type="file" name="myfile" style="display:none">
+                        </label>
                     </div>
                </div>
                <div class="more_info">
